b2af3e59d4587de03b4f6070bb16e249ba79fe35
Omar Alesharie <omar.alesharie@gmail.com>
2019-01-16 03:09:30 +0300

    Design and Script TheMain Menue

---
+++ Fighting-Survival/Assets/Scripts/GameSetting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameSetting : ScriptableObject
+{
+    public enum DifficultyLevel
+    {
+        Easy,Normal,Hard
+    }
+
+    public enum CameraType
+    {
+        Free,Follow
+    }
+
+    private DifficultyLevel difficultyLevel = DifficultyLevel.Easy;
+    private CameraType cameraType = CameraType.Free;
+
+    public void Set_DifficultyLevel(DifficultyLevel difficulty)
+    {
+        difficultyLevel = difficulty;
+    }
+
+    public void Set_CameraType(CameraType CamType)
+    {
+        cameraType = CamType;
+    }
+
+    public DifficultyLevel Get_DifficultyLevel()
+    {
+        return difficultyLevel;
+    }
+
+    public CameraType Get_CameraType()
+    {
+        return cameraType;
+    }
+}
--- Fighting-Survival/Assets/Scripts/MainMenu.cs
+++ Fighting-Survival/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameSetting Setting;
+
+    public Toggle EasyToggle;
+    public Toggle NormalToggle;
+    public Toggle HardToggle;
+
+    public Toggle FreeCameraToggle;
+    public Toggle FollowCameraToggle;
+
+
+
+    private void Start()
+    {
+        SetOptionsUI();
+    }
+
+    public void EasySelected()
+    {
+        Setting.Set_DifficultyLevel(GameSetting.DifficultyLevel.Easy);
+    }
+
+    public void NormalSelected()
+    {
+        Setting.Set_DifficultyLevel(GameSetting.DifficultyLevel.Normal);
+    }
+
+    public void HardSelected()
+    {
+        Setting.Set_DifficultyLevel(GameSetting.DifficultyLevel.Hard);
+    }
+
+    public void FreeCameraSelected()
+    {
+        Setting.Set_CameraType(GameSetting.CameraType.Free);
+    }
+
+    public void FollowCameraSelected()
+    {
+        Setting.Set_CameraType(GameSetting.CameraType.Follow);
+    }
+
+    void SetOptionsUI()
+    {
+        GameSetting.DifficultyLevel difficultyLevel = Setting.Get_DifficultyLevel();
+        switch (difficultyLevel)
+        {
+            case GameSetting.DifficultyLevel.Easy:
+                EasyToggle.isOn = true;
+                break;
+            case GameSetting.DifficultyLevel.Normal:
+                NormalToggle.isOn = true;
+                break;
+            case GameSetting.DifficultyLevel.Hard:
+                HardToggle.isOn = true;
+                break;
+            default:
+                break;
+        }
+
+        GameSetting.CameraType cameraType = Setting.Get_CameraType();
+        switch (cameraType)
+        {
+            case GameSetting.CameraType.Free:
+                FreeCameraToggle.isOn = true;
+                break;
+            case GameSetting.CameraType.Follow:
+                FollowCameraToggle.isOn = true;
+                break;
+            default:
+                break;
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
