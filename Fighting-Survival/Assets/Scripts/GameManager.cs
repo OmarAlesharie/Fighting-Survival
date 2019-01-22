db063765096648122d40a11678eee520d54213a9
Omar Alesharie <omar.alesharie@gmail.com>
2019-01-22 19:13:19 +0300

    Adding Screenshots

--- Fighting-Survival/Assets/Scripts/GameManager.cs
+++ Fighting-Survival/Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
     private bool Paused;
 
     GameSetting.DifficultyLevel difficultyLevel;
-    
+  
     // Start is called before the first frame update
     void Start()
     {
